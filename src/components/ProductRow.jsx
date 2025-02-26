7757d86edd7c021bd7184099104af2d1f943964c
awnetcode <155958835+awnetcode@users.noreply.github.com>
2025-02-26 19:07:47 +0100

    component-structure

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -1,23 +1,28 @@
 import './styles/productRow.css';
 
-import products from '../assets/data.json';
+import PropTypes from 'prop-types';
 
-
-const ProductRow = () =>{
+const ProductRow = ({productsArray}) =>{
     
-    //console.log(products.products);
-     
+
+     console.log(productsArray)
 
     return(
         <>
-        <ul>
-            {products.products.map((item, index) =>{
-                    <li key={index}>lol</li>
-                })
-}
+        <ul className='product-list'>
+        {productsArray.map((product, index) => (
+                <li key={index} className='product-item'>
+                    <span>{product.name}</span>
+                    <span>{product.price}</span>
+                </li>
+            ))}
         </ul>
         </>
     )
+}
+
+ProductRow.propTypes = {
+    productsArray: PropTypes.arrayOf(PropTypes.object).isRequired
 }
 
 export default ProductRow;
--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,19 +1,31 @@
 import './styles/productTable.css';
 
+import { useState, useEffect } from 'react';
+
+
+import data from '../assets/data.json';
+
 import ProductCategoryRow from './ProductCategoryRow';
 import ProductRow from './ProductRow';
 
 const ProductTable = () =>{
 
+    const [productsArray, setProductsArray] = useState([]);
+
+   useEffect(()=>{
+    setProductsArray(data.products);
+    },[]);
 
 
 
     return(
         <>
         <ProductCategoryRow/>
-        <ProductRow/>
+        <ProductRow productsArray={productsArray}/>
         </>
     )
 }
+
+
 
 export default ProductTable;
